c4f790c76733b1e43f2dd896e7c80463e7d0e3cb
Rock <dhanjee.don@gmail.com>
2024-11-17 11:52:43 +0530

    first commit

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int ayush = 1284679546;
+    ayush = 748837;
+    ayush = 1222;
+
+    int rock;
+
+    const int cantUpdate = 999;
+    // the variable declared with const keyword can't be updated or changed; if you'll update it will give error;
+    // cantUpdate = 0;
+
+    // IMPORTANT** int,const,for,cout,cin these keywords are known as IDENTIFIERS nd there are others too....
+    // you can't use them as a variable name
+
+
+    cout << cantUpdate << endl;
+
+
+    // cout << "Welcome to C++ with Rock 1" << endl;
+    // cout << "Welcome to C++ with Rock 2" <<endl;
+    // cout << "Welcome to C++ with Rock 3" <<endl;
+    // cout << "Welcome to C++ with Rock 4" <<endl;
+
+    rock = 10;
+
+    cout << ayush << endl;
+    cout << rock <<endl;
+
+    return 0;
+}
---
+++ test/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// using std::cout;
+// we can use one by one namespace like this 
+// using std::endl;
+
+using namespace std;
+// or we can use whole reference at once by using namespace
+
+
+int main(){
+    cout << "Hello World, This is my first C++ code" <<endl;
+    return 0;
+}
+
+
